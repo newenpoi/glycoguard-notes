f7eddfa8a44f9cd7fc64e9f34eed6d76a13bd4b9
newenpoi <lammenschristopher119@hotmail.com>
2024-01-27 15:00:59 +0100

    security config

--- src/main/java/com/openclassrooms/medilabo/glycoguardnotes/security/SecurityConfig.java
+++ src/main/java/com/openclassrooms/medilabo/glycoguardnotes/security/SecurityConfig.java
@@ -2,29 +2,46 @@ package com.openclassrooms.medilabo.glycoguardnotes.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
+    
+	@Bean
+    public static PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+	
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-		return http.csrf(csrf -> csrf.disable())
-				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-				.authorizeHttpRequests(auth -> auth
-						.requestMatchers(new AntPathRequestMatcher("/"),
-								new AntPathRequestMatcher("/actuator/refresh"),
-								new AntPathRequestMatcher("/notes/**"),
-								new AntPathRequestMatcher("/swagger-ui/**"))
-						.permitAll().requestMatchers(new AntPathRequestMatcher("/admin/**")).authenticated())
-				.build();
+		http.csrf(csrf -> csrf.disable()).authorizeHttpRequests((authorize) -> {
+			authorize.anyRequest().authenticated();
+		}).httpBasic(Customizer.withDefaults());
+		
+		return http.build();
 	}
+	
+    @Bean
+    public UserDetailsService userDetailsService(){
+
+        UserDetails admin = User.builder()
+                .username("newenpoi")
+                .password(passwordEncoder().encode("azerty"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin);
+    }
 	
 	@Bean
 	public WebSecurityCustomizer webSecurityCustomizer() {
